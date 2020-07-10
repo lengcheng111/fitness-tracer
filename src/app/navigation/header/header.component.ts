c9c686a444ad88524f708265ef1214cac3528268
truong <vle@tibco.com>
2020-07-10 13:51:57 +0700

    auth service

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { AuthService } from './../../auth/auth.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(data => {
+      this.isAuth = data;
+    });
   }
 
   onToggleSidenav() {
